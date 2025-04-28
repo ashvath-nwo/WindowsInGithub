dc7a19ae16752520e4816ed2e7940c1ea386e059
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-04-28 22:30:15 +0530

    Update DockerContainer.sh

--- scripts/DockerContainer.sh
+++ scripts/DockerContainer.sh
@@ -1,6 +1,6 @@
 sudo su
-apt update
-apt install docker.io
+sudo apt update
+sudo apt install docker.io
 echo Do not worry if it says it has unmet depencities 
 wget https://raw.githubusercontent.com/dockur/windows/refs/heads/master/compose.yml
-docker-compose -f compose.yml up
+sudo docker-compose -f compose.yml up
